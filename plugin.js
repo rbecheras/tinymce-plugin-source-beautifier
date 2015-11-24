e48baca618bed570fe3d8de439daa8f0d230228b
Rémi Becheras <rbecheras@sirap.fr>
2015-11-24 09:05:45 +0100

    init plugin main file

---
+++ plugin.js
@@ -0,0 +1,17 @@
+/**
+ * plugin.js
+ *
+ * Released under LGPL License.
+ * Copyright (c) 2015 SIRAP SAS All rights reserved
+ *
+ * License: http://www.tinymce.com/license
+ * Contributing: http://www.tinymce.com/contributing
+ */
+
+/*global tinymce:true */
+
+tinymce.PluginManager.add('indent', function(editor) {
+	/**
+	 * @TODO
+	 */
+});
